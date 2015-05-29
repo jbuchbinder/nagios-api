d4e956f019f7ae9000f1b5dacb7e3cc97f864f56
Jeff Buchbinder <jeff@ourexchange.net>
2015-05-29 11:11:22 -0400

    Begin to add methods to deal with multi-service queries.

---
+++ util.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+const (
+	nameServiceSeparator = ":"
+	nameServicesSeparator = ","
+)
+
+type hostServicePair struct {
+	Host    string
+	Service string
+}
+
+func newHostServicePair(str string) (hostServicePair, error) {
+	if strings.Index(str, nameServiceSeparator) == -1 {
+		return hostServicePair{}, errors.New("Invalid format")
+	}
+	slices := strings.Split(str, nameServiceSeparator)
+	return hostServicePair{
+		Host:    slices[0],
+		Service: slices[1],
+	}, nil
+}
+
+func newHostServicePairs(str string) ([]hostServicePair, error) {
+	if strings.Index(str, nameServicesSeparator) == -1 {
+		// Process as single entry
+		p, e := newHostServicePair(str)
+		return []hostServicePair{p}, e
+	}
+	slices := strings.Split(str, nameServicesSeparator)
+	hsp := make([]hostServicePair, len(slices))
+	var e error
+	for i := range slices {
+		hsp[i], e = newHostServicePair(strings.TrimSpace(slices[i]))
+		if e != nil {
+			return []hostServicePair{}, e
+		}
+	}
+	return hsp, nil
+}
--- web.go
+++ web.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func buildRouter() *mux.Router {
@@ -18,6 +19,7 @@ func buildRouter() *mux.Router {
 	r_services := r.PathPrefix("/services").Subrouter()
 	r_services.HandleFunc("/{host}", listServiceHandler).Methods("GET")
 	r_services.HandleFunc("/{host}/{service}", serviceHandler).Methods("GET")
+	r_services.HandleFunc("/{host}/{service}/state", serviceStateHandler).Methods("GET")
 
 	return r
 }
@@ -59,12 +61,28 @@ func serviceHandler(w http.ResponseWriter, r *http.Request) {
 	writeOutput(w, h.ServiceStatus[vars["service"]])
 }
 
+func serviceStateHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	log.Printf("serviceStateHandler: %s: %s/%s", r.RemoteAddr, vars["host"], vars["service"])
+	h := status.HostStatus[vars["host"]]
+	i, e := strconv.Atoi(h.ServiceStatus[vars["service"]].Current_state)
+	if e != nil {
+		writeError(w, e)
+		return
+	}
+	writeOutput(w, i)
+}
+
+func writeError(w http.ResponseWriter, e error) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(501)
+	w.Write([]byte(fmt.Sprintf("{\"error\":\"%s\"}", e)))
+}
+
 func writeOutput(w http.ResponseWriter, obj interface{}) {
 	b, e := json.Marshal(obj)
 	if e != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(501)
-		w.Write([]byte(fmt.Sprintf("{\"error\":\"%s\"}", e)))
+		writeError(w, e)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
